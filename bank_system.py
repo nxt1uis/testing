713afb5b638d181e432690b3340e3d582ecece8d
Luis <lute3188@student.harmonytx.org>
2024-04-28 20:53:13 -0500

    finishing touched

--- bank_system.py
+++ bank_system.py
@@ -1,49 +1,181 @@
-import tkinter as tk
+from mySQL_API import *
 
-def login():
-    username = entry_username.get()
-    password = entry_password.get()
-    
-    # Add your login logic here
-    
-    if username == "admin" and password == "password":
-        label_result.config(text="Login successful!")
+accountnumber = 0
+
+def print_header():
+    #just for decoration
+    print('\n************************************************************')
+
+def create_new_user_account():
+    #creates new account if there are no users in the database
+    new_account_name = input('\nwhat is the name of the new account? ')
+    new_account_lastname = input('what is the last name? ')
+    new_account_address = input('what is the address? ')
+    new_account_pin = input('what is the pin? ')
+    accountnumber = createaccount(new_account_name, new_account_lastname, new_account_address, new_account_pin)
+    print(f"Account created successfully. Your new account's number is: {accountnumber}")
+    input("go back? ")
+    return 'standard_menu'
+
+def login_menu():
+    #this menu allows you to log into either a customer or admin account
+    #to do this, you have to enter your account number and pin
+    global accountnumber
+    next_state = ""
+    print("\nWelcome to the bank system. Please enter your account number and pin.\n")
+    accountnumber = int(input("Account number? "))
+    db_pin = get_pin(accountnumber)
+
+    if(db_pin == None):
+        user_new_account = input("Account number does not exist. Would you like to make a new one? ")
+        if user_new_account == "yes":
+            return "create_new_user_account"
+        else:
+            return "login_menu"
+
+    pin = int(input("PIN? "))
+    usertype = get_user_type(accountnumber)
+
+    if (pin == db_pin) and (usertype == "admin"):
+        print('\nWelcome to the admin menu')
+        next_state = 'admin_menu'
+    elif (pin == db_pin) and (usertype == "customer"):
+        print("\nYou have logged in successfuilly")
+        next_state= 'standard_menu'
     else:
-        label_result.config(text="Login failed!")
+        input("PIN nummber is incorrect.")
+        next_state= 'login_menu'
 
-# Create the main window
-window = tk.Tk()
-window.title("Login")
-window.geometry("300x200")
+    return next_state
 
+def standard_menu():
+    #standard customer menu with all of the customer options
+    #A to check balance
+    #B to withdraw from your account
+    #C to deposit into your account
+    #Q to quit program
+    options = {"a":"check_balance_menu","b":"withdraw_menu", "c":"deposit_menu","q":"quit"}
+    print("\nselect your options")
+    print("a - Check Balance")
+    print("b - Withdraw")
+    print("c - Deposit")
+    print("q - quit")
+    return options[input("option? ")]
 
+def check_balance_menu():
+    #menu that pops up when you type A in the main menu
+    #it shows you your total balance on your bank account
+    global accountnumber
+    balance = get_transactions(accountnumber)
+    print(f"\nThe account number {accountnumber} has a balance of {balance}")
+    input("go back? ")
+    return 'standard_menu'
 
+def withdraw_menu():
+    #menu that pops up when you type B in the main menu
+    #it asks you how much you want to withdraw
+    withdraw = float(input ("\nHow much do you want to withdraw? "))
+    newbalance = create_transaction(accountnumber, -withdraw,"withdraw")
+    if(newbalance == None):
+        print("The transaction was NOT successful\n")
+        input("go back? ") 
+        return'standard_menu'
 
+    print(f"The transaction was successful. Your new balance is {newbalance}\n")
+    input("go back? ")  
+    return 'standard_menu'
 
-# add label    
-# Create the label
-label = tk.Label(window, text="Welcome to the Bank System")
-label.pack()
+def deposit_menu():
+    #menu that pops up when you type C in the main menu
+    #asks you how much money you want to deposit into your bank account
+    deposit = float(input ("\nHow much do you want to deposit? "))
+    newbalance = create_transaction(accountnumber, deposit, "deposit")
+    print(f"The transaction was successful. Your new balance is {newbalance}")
+    input("go back? ")  
+    return 'standard_menu'
+    
+def create_account():
+    #menu that pops up when you type A in the admin menu
+    #it asks you all of the necessary info to make a new account in the db
+    new_account_name = input('\nwhat is the name of the new account? ')
+    new_account_lastname = input('what is the last name? ')
+    new_account_address = input('what is the address? ')
+    new_account_pin = input('what is the pin? ')
+    newaccount = createaccount(new_account_name, new_account_lastname, new_account_address, new_account_pin)
+    print(f"Account created successfully. Your new account's number is: {newaccount}")
+    input("go back? ")
+    return 'admin_menu'
 
-# Create the username label and entry
-label_username = tk.Label(window, text="Username:")
-label_username.pack()
-entry_username = tk.Entry(window)
-entry_username.pack()
+def edit_account():
+    #menu that pops up when you type B in the main menu
+    #asks you what account you want to edit
+    #asks you what you want to edit in the account
+    account_to_edit = input("\nWhat's the account number you want to edit? ")
+    edit_options = {"a":"name", "b":"lastname", "c":"address", "d":"pin", "q":"quit"}
+    print("a - First Name")
+    print("b - Last Name")
+    print("c - address")
+    print("d - pin")
+    print("q - quit")
+    column = input("\nWhat do you want to change? ")
+    if column == "q":
+        return "admin_menu"
+    new_val = input('what do you want to change it to? ')
+    editaccount(account_to_edit, edit_options[column], new_val)
+    print('Account was successfully changed')
+    input("go back? ")
+    
+    return 'admin_menu'
 
-# Create the password label and entry
-label_password = tk.Label(window, text="Password:")
-label_password.pack()
-entry_password = tk.Entry(window, show="*")
-entry_password.pack()
+def delete_account():
+    #menu that pops up when you type C in the main menu
+    #asks you which account you want to delete from db
+    delete = input("\nwhich account would you like to delete? ")
+    deleteaccount(delete)
+    print(f"account delete successfully.")
+    input("go back? ")
+    return 'admin_menu'
 
-# Create the login button
-button_login = tk.Button(window, text="Login", command=login)
-button_login.pack()
+def admin_menu():
+    #admin menu with all of the admin options
+    #A to create a new account
+    #B to edit an account
+    #C to close an account
+    #Q to quit program
+    admin_options = {"a":"create_account", "b":"edit_account", "c":"delete_account", "q":"quit"}
+    print("\nselect your options")
+    print("a - Create new acccount")
+    print("b - Edit account")
+    print("c - Close account")
+    print("q - quit")
+    return admin_options[input("option? ")]
 
-# Create the result label
-label_result = tk.Label(window, text="")
-label_result.pack()
+if __name__ == "__main__":
+    state = "login_menu"
 
-# Start the main loop
-window.mainloop()
+    while True:
+        #main loop
+        print_header()
+        if(state == 'login_menu'):
+            state = login_menu()
+        if(state == 'create_new_user_account'):
+            state = create_new_user_account()
+        elif(state == 'standard_menu'):
+            state = standard_menu()
+        elif(state == 'admin_menu'):
+            state = admin_menu()
+        elif(state == 'create_account'):
+            state = create_account()
+        elif(state == 'delete_account'):
+            state = delete_account()
+        elif(state == 'edit_account'):
+            state = edit_account()
+        elif(state == 'check_balance_menu'):
+            state = check_balance_menu()
+        elif(state == 'withdraw_menu'):
+            state = withdraw_menu()      
+        elif(state == 'deposit_menu'):
+            state = deposit_menu()
+        elif(state == 'quit'):
+            break
+    print("\nhave a nice day\n")
--- mySQL_API.py
+++ mySQL_API.py
@@ -1,242 +1,135 @@
-
-
 import mysql.connector
 
 def establish_connection():
+    #establishes connection to mySQL
     connection = mysql.connector.connect(host='127.0.0.1', database='C2C database', user='root', password='luis')
     return connection
-
 
 def connect_to_mysql():
     # Establish a connection to the MySQL server
     connection = establish_connection()
-
-    # Check if the connection was successful
+    # Checks if the connection was successful
     if connection.is_connected():
         print("Connected to MySQL database")
-
-        # Close the connection
+        # Closes the connection
         connection.close()
         print("Connection closed")
 
 def createaccount(name, lastname, address, pin):
+    #creates account in the db 
     try:
-        # Establish a connection to the MySQL server
         connection = establish_connection()
-        # Create a cursor object to execute SQL queries
         cursor = connection.cursor()
-        
-        # Prepare the SQL query
         query = "INSERT INTO users (name, lastname, address, pin) VALUES (%s, %s, %s, %s)"
         values = (name, lastname, address, pin)
-        
-        # Execute the query
         cursor.execute(query, values)
-        
-        # Get the ID of the last inserted row
         last_id = cursor.lastrowid
-
-        # Commit the changes to the database
         connection.commit()
-        
-        # Close the cursor and connection
         cursor.close()
         connection.close()
-        
         return last_id
     except mysql.connector.Error as error:
         print("Error inserting data into MySQL table:", error)
 
 def editaccount(accountnum, col, val):
+    #edits an account in the db
     try:
-        # Establish the connection
         connection = establish_connection()
-
-        # Create a new cursor
         cursor = connection.cursor()
-
-        # The SQL query to update a column
-
         query = f"UPDATE `C2C database`.`users` SET `{col}` = '{val}' WHERE (`accountnum` = '{accountnum}');"
-
-        # Execute the query
-
         cursor.execute(query)
-
-        # Commit the changes to the database
         connection.commit()
-
         print("Data updated successfully")
-
     except mysql.connector.Error as error:
         print("Error updating data in MySQL table:", error)
-
     finally:
         if connection.is_connected():
             cursor.close()
             connection.close()
 
 def deleteaccount(accountnum):
+    #deletes account in the db
     try:
-        # Establish the connection
         connection = establish_connection()
-
-        # Create a new cursor
         cursor = connection.cursor()
-
-        # The SQL query to delete a row
         query = f"DELETE FROM users WHERE accountnum = {accountnum}"
-
-        # Execute the query
         cursor.execute(query)
-
-        # Commit the changes to the database
         connection.commit()
-
-        # The SQL query to delete a row
         query = f"DELETE FROM transactiontable WHERE accountnum = {accountnum}"
-
-        # Execute the query
         cursor.execute(query)
-
-        # Commit the changes to the database
         connection.commit()
-
-
-
     except mysql.connector.Error as error:
         print("Error deleting account in MySQL table:", error)
-
     finally:
         if connection.is_connected():
             cursor.close()
             connection.close()
 
 def create_transaction(accountnum, amount, transaction_type):
-
+    #creates transaction (withdraw or deposit) in the db
     balance = get_transactions(accountnum)
     if (float(balance) < abs(float(amount))) and (transaction_type == 'withdraw'):
         return None
-
     try:
-        # Establish the connection
         connection = establish_connection()
-
-        # Create a new cursor
         cursor = connection.cursor()
-
-        # The SQL query to insert a new row
         query = "INSERT INTO transactiontable (accountnum, amount, transactiontype) VALUES (%s, %s, %s)"
-
-        # Execute the query
         cursor.execute(query, (accountnum, amount, transaction_type))
-
-        # Get the ID of the last inserted row
         last_id = cursor.lastrowid
-
-        # Commit the changes to the database
         connection.commit()
-
-        #print("Transaction created successfully. ID is:", last_id)
         return get_transactions(accountnum)
-
     except mysql.connector.Error as error:
         print("Error creating transaction in MySQL table:", error)
-
     finally:
         if connection.is_connected():
             cursor.close()
             connection.close()
 
 def get_transactions(accountnum):
+    #gets transactions from the transactions table in the db to check balance
     try:
-        # Establish the connection
         connection = establish_connection()
-
-        # Create a new cursor
         cursor = connection.cursor()
-
-        # The SQL query to get all transactions for a given account number
         query = f"SELECT amount FROM transactiontable WHERE accountnum = '{accountnum}'"
-
-        # Execute the query
         cursor.execute(query)
-
-        # Fetch all the rows
         rows = cursor.fetchall()
-
         total = 0.0
         for row in rows:
-            #print(row)
             total=total+row[0]
-
-        #print(f'total amount is :{total}')
         return total
-              
-
     except mysql.connector.Error as error:
         print("Error getting transactions from MySQL table:", error)
-
     finally:
         if connection.is_connected():
             cursor.close()
             connection.close()
 
-def test_functions():
-    #connect_to_mysql()
-    #createaccount("Luis","terrazas","123 main","2090")
-    editaccount("6", "name", "antonio")
-    editaccount("6", "lastname", "mendoza")
-    editaccount("6", "pin", "1000") 
-    editaccount("6", "address", "456 main")
-    # deleteaccount("6")
-    create_transaction("6", "100", "deposit" )
-    create_transaction("6", "10", "deposit" )
-    create_transaction("6", "-15.54", "withdraw")
-    create_transaction("6", "-10", "withdraw")
-    print(get_transactions("6"))
-
 def get_pin(accountnum):
+    #gets the pin from the user table in the db for the login menu
     try:
-        # Establish the connection
         connection = establish_connection()
-
-        # Create a new cursor
         cursor = connection.cursor()
-
-        # The SQL query to get the pin for a given account number
         query = "SELECT pin FROM users WHERE accountnum = %s"
-
-        # Execute the query
         cursor.execute(query, (accountnum,))
-
-        # Fetch the first row
         row = cursor.fetchone()
-
         if row is not None:
             return row[0]
         else:
             return None
-
     except mysql.connector.Error as error:
         print("Error getting pin from MySQL table:", error)
-
     finally:
         if connection.is_connected():
             cursor.close()
             connection.close()
 
 def get_user_type(accountnum):
+    #gets the type of user in the user table to compare whether the user logging in is an admin or a customer
     try:
-        # Establish the connection
         connection = establish_connection()
-        # Create a new cursor
         cursor = connection.cursor()
-        # The SQL query to get the user type for a given account number
         query = "SELECT user_type FROM users WHERE accountnum = %s"
-        # Execute the query
         cursor.execute(query, (accountnum,))
-        # Fetch the first row
         row = cursor.fetchone()
         if row is not None:
             return row[0]
@@ -249,7 +142,20 @@ def get_user_type(accountnum):
             cursor.close()
             connection.close()
 
+def test_functions():
+    #test functions made in the making of the program
+    createaccount("Luis","terrazas","123 main","2090")
+    editaccount("6", "name", "antonio")
+    editaccount("6", "lastname", "mendoza")
+    editaccount("6", "pin", "1000") 
+    editaccount("6", "address", "456 main")
+    deleteaccount("6")
+    create_transaction("6", "100", "deposit" )
+    create_transaction("6", "10", "deposit" )
+    create_transaction("6", "-15.54", "withdraw")
+    create_transaction("6", "-10", "withdraw")
+    print(get_transactions("6"))
+    get_user_type("6")
+
 if __name__ == "__main__":
     test_functions()
-
-get_user_type("6")
